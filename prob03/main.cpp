629e6f18b7271d57300798dd23314d52ea5d9086
Jose Perez <joseperez@Joses-MacBook-Pro.local>
2019-09-21 14:28:35 -0700

    random rectangles

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,36 @@
 // Random Rectangle
+
+#include<iostream>
+#include<ctime>
+#include<string>
+#include<iomanip>
+#include<random>
+
+int main()
+{
+srand(time(0));
+//making variable
+int start = rand() % 40 + 1;
+std::string pound;
+pound.assign(start, '#');
+
+//output of random number
+std::cout<< pound << std:: endl;
+std::cout<< pound << std:: endl;
+std::cout<< pound << std:: endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
